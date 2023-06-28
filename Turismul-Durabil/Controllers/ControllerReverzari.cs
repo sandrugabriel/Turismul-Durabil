949f94eac4757031a88bb5f11f4cfc640fd436fc
Sandru Gabriel <gabi200611@yahoo.com>
2023-06-28 14:05:11 +0300

    done

---
+++ Turismul-Durabil/Controllers/ControllerReverzari.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Turismul_Durabil.Models;
+
+namespace Turismul_Durabil.Controllers
+{
+    internal class ControllerReverzari
+    {
+
+        private List<Rezervare> rezervari;
+
+        public ControllerReverzari()
+        {
+
+            rezervari = new List<Rezervare>();
+
+            load();
+
+        }
+
+        public void load()
+        {
+
+            string path = Application.StartupPath + @"/data/Rezervari.txt";
+
+            StreamReader streamReader = new StreamReader(path);
+
+            string t;
+
+            while((t = streamReader.ReadLine()) != null)
+            {
+
+                Rezervare rezervare = new Rezervare(t);
+                rezervari.Add(rezervare);
+            }
+
+            streamReader.Close();
+        }
+
+
+    }
+}
---
+++ Turismul-Durabil/Models/Rezervare.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turismul_Durabil.Models
+{
+    internal class Rezervare
+    {
+
+        private int IdRezervare;
+        private int IdUser;
+        private DateTime dateStart;
+        private DateTime dateEnd;
+        private int nrPersoane;
+        private double pret;
+
+        public Rezervare(int idRezervare, int idUser, DateTime dateStart, DateTime dateEnd, int nrPersoane, double pret)
+        {
+            this.IdRezervare = idRezervare;
+            this.IdUser = idUser;
+            this.dateStart = dateStart;
+            this.dateEnd = dateEnd;
+            this.nrPersoane = nrPersoane;
+            this.pret = pret;
+        }
+
+        public Rezervare(string t) {
+
+            string[] prop = t.Split('|');
+
+            this.IdRezervare = int.Parse(prop[0]);
+            this.IdUser = int.Parse(prop[1]);
+            this.dateStart = DateTime.Parse(prop[2]);
+            this.dateEnd = DateTime.Parse(prop[3]);
+            this.nrPersoane = int.Parse(prop[4]);
+            this.pret = int.Parse(prop[5]);
+        
+        }
+
+        public int getIdRezervare()
+        {
+            return this.IdRezervare;
+        }
+
+        public int getIdUser()
+        {
+            return this.IdUser;
+        }
+
+        public DateTime getDateStart()
+        {
+            return this.dateStart;
+        }
+
+        public DateTime getDateEnd()
+        {
+            return this.dateEnd;
+        }
+
+        public int getNrPersoane()
+        {
+            return this.nrPersoane;
+        }
+
+        public double getPret()
+        {
+            return this.pret;
+        }
+
+
+
+    }
+}
